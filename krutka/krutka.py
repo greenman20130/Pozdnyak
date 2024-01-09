5af5030374d7a1a867619b01225b8722e600db72
greenman20130 <deadbread228@gmail.com>
2024-01-09 20:30:18 +0300

    update
    improved performance

--- krutka/krutka.py
+++ krutka/krutka.py
@@ -1,23 +1,36 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from selenium.webdriver.chrome.service import Service as ChromeService
 import time
 import threading
+
+
+
 options = Options()
-options.add_argument('--headless')
-options.add_argument('--disable-gpu')
-options.add_argument('--no-sandbox')
+prefs = {'profile.default_content_setting_values': {'cookies': 2, 'images': 2,
+                            'plugins': 2, 'popups': 2, 'geolocation': 2, 
+                            'notifications': 2, 'auto_select_certificate': 2, 'fullscreen': 2, 
+                            'mouselock': 2, 'mixed_script': 2, 'media_stream': 2, 
+                            'media_stream_mic': 2, 'media_stream_camera': 2, 'protocol_handlers': 2, 
+                            'ppapi_broker': 2, 'automatic_downloads': 2, 'midi_sysex': 2, 
+                            'push_messaging': 2, 'ssl_cert_decisions': 2, 'metro_switch_to_desktop': 2, 
+                            'protected_media_identifier': 2, 'app_banner': 2, 'site_engagement': 2, 
+                            'durable_storage': 2}}
+options.add_experimental_option('prefs', prefs)
 options.add_argument('--disable-dev-shm-usage')
+caps = DesiredCapabilities().CHROME
+caps["pageLoadStrategy"] = "eager"
+service = ChromeService(desired_capabilities=caps)
 
 def test_logic():
-    driver = webdriver.Chrome(options=options)
+    driver = webdriver.Chrome(options=options, service=service)
     driver.get("https://rsport.ria.ru/20231219/boss-of-year-2023-1911702814.html")
-    time.sleep(2)
     try:
         no = driver.find_element(By.XPATH, '//*[@id="endless"]/div[1]/div/div/div/div[1]/div[1]/div/div[1]/div[1]/div/div/div/div[4]/div[2]/div[1]/div[1]').click()
         no = driver.find_element(By.XPATH, '//*[@id="endless"]/div/div/div/div/div[1]/div[1]/div/div[1]/div[1]/div/div/div/div[5]/div/div/div[2]/div/div[2]/div/div[2]/div/div/div[2]/div[1]/div[2]').click()
         no = driver.find_element(By.XPATH, '//*[@id="endless"]/div/div/div/div/div[1]/div[1]/div/div[1]/div[1]/div/div/div/div[6]/div/div/div[1]').click()
-        time.sleep(2)
         print("Голос отдан")
     except:
         driver.quit()
